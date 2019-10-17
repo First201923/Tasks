335a16cabaa223564108344bf5a4da8d0949b893
Ciarin404 <griiffon@gmail.com>
2019-10-17 14:21:54 +0300

    task 1 completed

---
+++ Module2_1/Module2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            byte M;
+            Console.Write("Введите число компаний:");
+            N = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите % налога:");
+            M = Byte.Parse(Console.ReadLine());
+            int income = 230;
+            float sum;
+            sum = N * income * M / 100; 
+            Console.WriteLine($"Суммарный налог государства:{sum}");
+            Console.ReadKey();
+        }
+    }
+}
