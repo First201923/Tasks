4ce8ec49281e000dc68f6831907f58650d79ca69
Ciarin404 <griiffon@gmail.com>
2019-10-17 14:24:13 +0300

    Task 2 completed

---
+++ Module2_2/Module2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte N;
+            Console.Write("Введите возраст:");
+            N = Byte.Parse(Console.ReadLine());
+            if ((N > 18) && (N % 2 == 0))
+                Console.WriteLine("Поздравляем с 18-и летием");
+            if ((N < 18) && (N > 13) && (N % 2 != 0))
+                Console.WriteLine("Поздравляем с переходом в старшую школу");
+            Console.ReadKey();
+        }
+    }
+}
