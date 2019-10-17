2c20639450b669e55ab8d45e7c7e1ce7b427139d
Ciarin404 <griiffon@gmail.com>
2019-10-17 14:26:18 +0300

    completed

---
+++ Module 2_3/Module 2_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Module_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float temp;
+            bool i = true;
+            while (i)
+            {
+                Console.WriteLine("Введите переменные a и b:");
+                if (!((float.TryParse(Console.ReadLine().Replace('.', ','), out float a))
+                    & (float.TryParse(Console.ReadLine().Replace('.', ','), out float b))))
+                {
+                    i = true;
+                    Console.Clear();
+                    Console.WriteLine("Вы ввели не числа. Попробуйте ещё раз.");
+                }
+                else
+                {
+                    i = false;
+                    temp = b;
+                    b = a;
+                    a = temp;
+                    Console.WriteLine($"a = {a}\nb = {b}");
+                }
+
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
