559c0ac552dd93ea165aadbb5edc5dbb17c593ad
Ciarin404 <griiffon@gmail.com>
2019-10-23 20:34:55 +0300

    fsefes

---
+++ Module3_7/Module3_7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Module3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random((int)Stopwatch.GetTimestamp());
+            Console.Write("Enter size of array:");
+            var stringSize = Console.ReadLine();
+            bool isNumber = int.TryParse(stringSize, out int sizeArray);
+            if (isNumber)
+            {
+                var numberArray = new int[sizeArray];
+
+                for (int i = 0; i < numberArray.Length; i++)
+                {
+                    numberArray[i] = rand.Next(-10, 10);
+                    Console.Write($"{numberArray[i]} ");
+                }
+
+                Console.Write("\nNumbers are greater than next: ");
+
+                for (int i = 0; i < numberArray.Length - 1; i++)
+                {
+                    if (numberArray[i] > numberArray[i + 1])
+                    {
+                        Console.Write($"{numberArray[i]} ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
