0c08fcfcc305f4eca75feca1bde36f058282412d
Ciarin404 <griiffon@gmail.com>
2019-10-23 20:33:21 +0300

    cecse

---
+++ Module3_6/Module3_6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Module3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random((int)Stopwatch.GetTimestamp());
+            Console.Write("Enter size of Array:");
+            var stringSize = Console.ReadLine();
+            bool isNumber = int.TryParse(stringSize, out int sizeArray);
+            var numberArray = new int[sizeArray];
+
+            if (isNumber)
+            {
+                for (int i = 0; i < numberArray.Length; i++)
+                {
+                    numberArray[i] = rand.Next(-10, 10);
+                    Console.Write($"{numberArray[i]} ");
+                }
+
+                Console.WriteLine("\nArray after conversion.");
+
+                for (int i = 0; i < numberArray.Length; i++)
+                {
+                    numberArray[i] = -numberArray[i];
+                    Console.Write($"{numberArray[i]} ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
