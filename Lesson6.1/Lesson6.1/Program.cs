f607b66412c4c4ab53d24060e0274b447945bdf9
Ciarin404 <griiffon@gmail.com>
2019-10-23 20:42:04 +0300

    dwa

---
+++ Lesson6.1/Lesson6.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team();
+            team[0] = new Player { Name = "Ronaldo", Number = 9 };
+            team[1] = new Player { Name = "R. Baggio", Number = 10 };
+            team[12] = new Player { Name = "Simeone", Number = 8 };
+            Console.WriteLine($"{team[12].Name} {team[12].Number}");
+            Console.ReadKey();
+        }
+    }
+    class Player
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    class Team
+    {
+        Player[] players = new Player[11];
+        public Player this[int id]
+        {
+            get
+            {
+                if (id >= 0 && id < players.Length)
+                    return players[id];
+                else
+                    return null ;
+            }
+            set
+            {
+                if (id >= 0 && id < players.Length)
+                    players[id] = value;
+            }
+        }
+    }
+}
